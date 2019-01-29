eca7c86c395d3e9364a8bc51456397ef205c3de7
Tedd Mason <teddmason@gmail.com>
2019-01-29 08:35:20 +0000

    Travis setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.com/DEFRA/fws-api.svg?token=gaJqX8fxhoSAADGJKMvM&branch=master)](https://travis-ci.com/DEFRA/fws-api)
+
 # FWS-API
---
+++ test/functions/test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+const Code = require('code')
+
+lab.experiment('test', () => {
+  lab.test('test1', () => {
+    Code.expect(1).to.equal(1)
+  })
+})
