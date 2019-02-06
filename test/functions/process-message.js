8a5bb2c9d379ff4327802674bc64dc16ce4628eb
Niki <niki.wycherley@environment-agency.gov.uk>
2019-02-06 14:15:40 +0000

    start of building unit tests

---
+++ test/functions/process-message.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+const Code = require('code')
+const handler = require('../../lib/functions/process-message').handler
+
+const Db = require('../../lib/helpers/db')
+const Services = require('../../lib/helpers/services')
+const Message = require('../../lib/models/message')
+
+const sinon = require('sinon').createSandbox()
+
+lab.experiment('test', () => {
+  lab.beforeEach(async () => {
+    // setup mocks
+    sinon.stub(Db.prototype, 'getLastMessage').callsFake(() => {
+      return Promise.resolve({})
+    })
+    lab.test('test1', () => {
+      Code.expect(1).to.equal(1)
+    })
+  })
+})
