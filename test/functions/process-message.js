190b712520c07a80ad1aeb3ca8e834dd843ef608
Niki <niki.wycherley@environment-agency.gov.uk>
2019-04-03 11:34:01 +0100

    removed unused getLastMessage call and changed Error to be a generic error thrown

--- test/functions/process-message.js
+++ test/functions/process-message.js
@@ -186,7 +186,7 @@ lab.experiment('Notification API Client', () => {
         }]
       })
     })
-    sinon.stub(services.prototype, 'putMessage').throws(new Error('insert or update on table "message" violates foreign key constraint "fk_tacode_message_target_area"'))
+    sinon.stub(services.prototype, 'putMessage').throws(new Error())
     try {
       await handler({
         bodyXml: '<?xml version="1.0" encoding="UTF-8"?><WarningMessage xmlns="http://www.environment-agency.gov.uk/XMLSchemas/EAFWD" approved="12/10/2018 13:29" requestId="" language="English"><TargetAreaCode><![CDATA[111FWCECD022]]></TargetAreaCode><SeverityLevel>2</SeverityLevel><InternetSituation><![CDATA[ This warning is in place for Preston Beach, Weymouth with tides at their highest between 7:30pm and 9:30pm today Friday 12th October. Flooding may occur along Preston Beach road. Large waves and spray mixed with shingle are likely so take care near coastal paths and promenades. The highest forecast water level including waves is 4mAOD, this is 3.34 metres above astronomical tide level. The forecast wind direction is SSW and the forecast wind strength is Force 7. Coastal conditions should ease for Saturday\'s high tides, however we are continuing to monitor the situation.]]></InternetSituation><FWISGroupedTACodes><![CDATA[]]></FWISGroupedTACodes></WarningMessage>'
@@ -197,21 +197,7 @@ lab.experiment('Notification API Client', () => {
     }
   })
   lab.test(' 6 - getting error when database is unavailible', async () => {
-    sinon.stub(services.prototype, 'getLastMessage').callsFake(() => {
-      return Promise.resolve({
-        rows: [{
-          id: '153',
-          message_received: 'Fri Mar 01 2019 15:01:54 GMT+0000 (GMT)',
-          severity: 'Flood Warning',
-          severity_changed: 'Mon Dec 10 2018 13:29:00 GMT+0000 (GMT)',
-          severity_value: '2',
-          situation: ' This warning is in place for Preston Beach, Weymouth with tides at their highest between 7:30pm and 9:30pm today Friday 12th October. Flooding may occur along Preston Beach road. Large waves and spray mixed with shingle are likely so take care near coastal paths and promenades. The highest forecast water level including waves is 4mAOD, this is 3.34 metres above astronomical tide level. The forecast wind direction is SSW and the forecast wind strength is Force 7. Coastal conditions should ease for Saturday\'s high tides, however we are continuing to monitor the situation.',
-          situation_changed: 'Mon Dec 10 2018 13:29:00 GMT+0000 (GMT)',
-          target_area_code: '666'
-        }]
-      })
-    })
-    sinon.stub(services.prototype, 'putMessage').throws(new Error('getaddrinfo ENOTFOUND fws-dev-rds.aws-int.defra.cloud fws-dev-rds.aws-int.defra.cloud:5432'))
+    sinon.stub(services.prototype, 'putMessage').throws(new Error())
     try {
       await handler({
         bodyXml: '<?xml version="1.0" encoding="UTF-8"?><WarningMessage xmlns="http://www.environment-agency.gov.uk/XMLSchemas/EAFWD" approved="12/10/2018 13:29" requestId="" language="English"><TargetAreaCode><![CDATA[111FWCECD022]]></TargetAreaCode><SeverityLevel>2</SeverityLevel><InternetSituation><![CDATA[ This warning is in place for Preston Beach, Weymouth with tides at their highest between 7:30pm and 9:30pm today Friday 12th October. Flooding may occur along Preston Beach road. Large waves and spray mixed with shingle are likely so take care near coastal paths and promenades. The highest forecast water level including waves is 4mAOD, this is 3.34 metres above astronomical tide level. The forecast wind direction is SSW and the forecast wind strength is Force 7. Coastal conditions should ease for Saturday\'s high tides, however we are continuing to monitor the situation.]]></InternetSituation><FWISGroupedTACodes><![CDATA[]]></FWISGroupedTACodes></WarningMessage>'
