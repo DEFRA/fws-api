145b4969d394a622b8fbd53d91a78cb7a20bca2e
Tedd Mason <teddmason@gmail.com>
2019-08-29 08:41:57 +0100

    unit-test and package update

--- test/functions/process-message-sad.js
+++ test/functions/process-message-sad.js
@@ -105,6 +105,20 @@ lab.experiment('Notification API Client', () => {
     }
   })
   lab.test(' 5 - getting error when database is unavailible', async () => {
+    sinon.stub(services.prototype, 'getLastMessage').callsFake(() => {
+      return Promise.resolve({
+        rows: [{
+          id: '153',
+          message_received: 'Fri Mar 01 2019 15:01:54 GMT+0000 (GMT)',
+          severity: 'Flood Warning',
+          severity_changed: 'Mon Dec 10 2018 13:29:00 GMT+0000 (GMT)',
+          severity_value: '2',
+          situation: ' This warning is in place for Preston Beach, Weymouth with tides at their highest between 7:30pm and 9:30pm today Friday 12th October. Flooding may occur along Preston Beach road. Large waves and spray mixed with shingle are likely so take care near coastal paths and promenades. The highest forecast water level including waves is 4mAOD, this is 3.34 metres above astronomical tide level. The forecast wind direction is SSW and the forecast wind strength is Force 7. Coastal conditions should ease for Saturday\'s high tides, however we are continuing to monitor the situation.',
+          situation_changed: 'Mon Dec 10 2018 13:29:00 GMT+0000 (GMT)',
+          target_area_code: '666'
+        }]
+      })
+    })
     sinon.stub(services.prototype, 'putMessage').throws(new Error())
     try {
       await handler({
