ba5c5eef1178a9d3ee620c3837ed407301e6987f
John Shields <John.Shields@environment-agency.gov.uk>
2019-10-01 13:23:35 +0100

    FIS-233: Add new lambda function storing warnings and alerts in S3 bucket.

---
+++ test/functions/put-warnings-alerts.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const Lab = require('@hapi/lab')
+const lab = exports.lab = Lab.script()
+const Code = require('@hapi/code')
+const handler = require('../../lib/functions/put-warnings-alerts').handler
+const eventJson = require('../data/events/get-all-messages-json.json')
+const Services = require('../../lib/helpers/services')
+const AWS = require('aws-sdk')
+const Db = require('../../lib/helpers/db')
+
+const sinon = require('sinon').createSandbox()
+
+lab.experiment('functions/store-warnings-alerts', () => {
+  lab.beforeEach(() => {
+    // setup mocks
+
+    sinon.stub(Db.prototype, 'query').callsFake((query) => {
+      return Promise.resolve({})
+    })
+    sinon.stub(Services.prototype, 'getAllMessages').callsFake(() => {
+      return Promise.resolve({
+        rows: []
+      })
+    })
+    sinon.stub(Services.prototype, 'clearDownNoLongerInForce').callsFake(() => {
+      return Promise.resolve({})
+    })
+  })
+
+  lab.afterEach(() => {
+    sinon.restore()
+  })
+
+  lab.test('store all warnings/alerts - success', async () => {
+    const putObjectStub = AWS.S3.prototype.putObject = sinon.stub()
+    putObjectStub.yields(null, 'Success')
+    const response = await handler(eventJson)
+
+    Code.expect(response).to.equal('Success')
+  })
+
+  lab.test('store all warnings/alerts - error', async () => {
+    const putObjectStub = AWS.S3.prototype.putObject = sinon.stub()
+    putObjectStub.yields('failure', null)
+    let result
+    try {
+      result = await handler(eventJson)
+    } catch (err) {
+      result = err
+    }
+
+    // Code.expect(await handler(eventJson)).to.reject()
+    Code.expect(result).to.equal('failure')
+  })
+
+  lab.test('store all warnings/alerts - catch error', async () => {
+    const putObjectStub = AWS.S3.prototype.putObject = sinon.stub()
+    putObjectStub.throws(new Error('Catch error'))
+    let result
+    try {
+      result = await handler(eventJson)
+    } catch (err) {
+      result = err
+    }
+    Code.expect(result.message).to.equal('Catch error')
+  })
+})
