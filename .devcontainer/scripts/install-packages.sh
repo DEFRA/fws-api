737a7865c52ebd79a5dc21c0215dd1b92ecb3624
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-07 11:53:23 +0000

    Default to Node.js 18

--- .devcontainer/scripts/install-packages.sh
+++ .devcontainer/scripts/install-packages.sh
@@ -3,6 +3,7 @@ set -e
 # Install Nodesource package signing key and the Node.js repository.
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --yes --dearmor -o /usr/share/keyrings/nodesource.gpg
 #install -o root -g root -m 644 nodesource.gpg /etc/apt/trusted.gpg.d/ && rm nodesource.gpg
-echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODEJS_VERSION}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODEJS_VERSION:-18}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
 # Install packages
 apt update -y && apt install -y netcat nodejs && apt clean all -y
+
