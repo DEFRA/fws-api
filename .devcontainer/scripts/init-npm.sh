5780d94cfc9d833ef65a358789424ff37c072f74
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-07 15:02:32 +0000

    Initialise NPM

---
+++ .devcontainer/scripts/init-npm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# Allow npm package installation without using sudo
+( echo NPM_PACKAGES="${HOME}/.npm-packages"; echo PATH="${HOME}/.npm-packages/bin:${PATH}") >> ${HOME}/.bashrc
+
+# Install npm packages
+#npm i
+
