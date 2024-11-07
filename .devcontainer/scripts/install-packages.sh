5cd9336c63ddc86c699fe9e6b607a271276dea5c
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-07 15:43:27 +0000

    Use post create command and tidy scripting

--- .devcontainer/scripts/install-packages.sh
+++ .devcontainer/scripts/install-packages.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove /etc/apt/sources.list.d/backports.list to resolve apt duplication warnings
-rm /etc/apt/sources.list.d/backports.list
+rm -f /etc/apt/sources.list.d/backports.list
 # Install Nodesource package signing key and the Node.js repository.
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --yes --dearmor -o /usr/share/keyrings/nodesource.gpg
 echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODEJS_VERSION}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
