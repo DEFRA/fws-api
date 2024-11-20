0db3c57f6df8cce725a61cbffd6c07fa7d045943
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-20 16:59:19 +0000

    Force docker image prune

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -6,4 +6,4 @@ sudo .devcontainer/scripts/install-packages.sh
 .devcontainer/scripts/install-maven.sh
 .devcontainer/scripts/apply-liquibase-changesets.sh
 .devcontainer/scripts/init-npm.sh
-docker image prune
+docker image prune -f
