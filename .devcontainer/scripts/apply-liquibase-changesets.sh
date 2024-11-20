4661889601c2f44771e2543056190bf8a47de7f7
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-20 15:37:29 +0000

    Apply Liquibase changesets to fws_db

---
+++ .devcontainer/scripts/apply-liquibase-changesets.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+cp docker/infrastructure/fws-db/pom.xml ../fws-db/u_fws/pom.xml
+
+export FWS_DB_USERNAME=$(cat /run/secrets/fws_db_username)
+export FWS_DB_PASSWORD=$(cat /run/secrets/fws_db_password)
+export FWS_DB_URL=$(cat /run/secrets/fws_db_url)
+
+# Ensure mvn is on the PATH.
+. ~/.profile
+(cd ../fws-db/u_fws && mvn liquibase:update)
