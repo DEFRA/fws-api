bfb47f8e97d14b9314012d1ee2ca932d339639a2
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-20 15:37:29 +0000

    Apply Liquibase changesets to fws_db

--- .devcontainer/scripts/install-packages.sh
+++ .devcontainer/scripts/install-packages.sh
@@ -7,5 +7,6 @@ rm -f /etc/apt/sources.list.d/backports.list
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --yes --dearmor -o /usr/share/keyrings/nodesource.gpg
 echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODEJS_VERSION}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
 # Install packages
-apt update -y && apt install -y netcat nodejs && apt clean all -y
-
+apt update -y && \
+apt install -y openjdk-21-jdk netcat nodejs && \
+apt clean -y
