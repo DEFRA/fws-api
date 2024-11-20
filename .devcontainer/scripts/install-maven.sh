4e371f744712f05b8302bfaadc6346a27822e670
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-20 16:49:01 +0000

    Use default keyserver

--- .devcontainer/scripts/install-maven.sh
+++ .devcontainer/scripts/install-maven.sh
@@ -7,9 +7,9 @@ MAVEN_VERSION=3.9.9
 # See https://maven.apache.org/download.cgi?.
 
 # Import the Apache Maven public key from the Ubuntu key server.
-echo Importing Apache Maven public key - If this hangs try waiting a few minutes attempting again.
-gpg --keyid-format long --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ${APACHE_MAVEN_PUBLIC_KEY}
-echo Imported Apache Maven public key
+echo Importing Apache Maven public key ${APACHE_MAVEN_PUBLIC_KEY}
+gpg --keyid-format long --recv-keys ${APACHE_MAVEN_PUBLIC_KEY}
+echo Imported Apache Maven public key ${APACHE_MAVEN_PUBLIC_KEY}
 mkdir -p ${DOWNLOAD_DIR}
 curl "https://dlcdn.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz" -o ${DOWNLOAD_DIR}apache-maven-${MAVEN_VERSION}-bin.tar.gz
 curl "https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz.asc" -o ${DOWNLOAD_DIR}apache-maven-${MAVEN_VERSION}-bin.tar.gz.asc
