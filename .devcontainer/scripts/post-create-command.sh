cd05e2013385ed8b28e66b8eca88e06a8ed452cc
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-26 11:02:50 +0000

    Rename Lambda function shell script

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -8,5 +8,5 @@ sudo .devcontainer/scripts/install-packages.sh
 .devcontainer/scripts/apply-liquibase-changesets.sh
 docker exec -it fws-api_devcontainer-fwsdb-1 psql -d $(cat /run/secrets/fws_db_name) -f /tmp/populate-api-keys.sql
 .devcontainer/scripts/init-npm.sh
-.devcontainer/scripts/create-lambda-functions.sh
+.devcontainer/scripts/register-lambda-functions.sh
 docker image prune -f
