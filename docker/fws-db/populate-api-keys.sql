4402e8e14af7e5a40ec8852035aceb04e6d0bbff
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-26 12:58:16 +0000

    Address SonarCloud issues

--- docker/fws-db/populate-api-keys.sql
+++ docker/fws-db/populate-api-keys.sql
@@ -1,7 +1,12 @@
-TRUNCATE TABLE u_fws.api_key;
+DO $$
+DECLARE
+  modified_by CONSTANT TEXT := 'developer';
+BEGIN
+  TRUNCATE TABLE u_fws.api_key;
 
-INSERT INTO u_fws.api_key(account_id, key, account_name, read, write, date_created, date_modified, modified_by) VALUES 
-  ('dev-write', uuid_generate_v4(), 'Read and write account for local dev', true, true, '2024-11-25 16:10:30', '2024-11-25 16:10:30', 'developer'),
-  ('dev-read', uuid_generate_v4(), 'Read account for local dev', true, false, '2024-11-25 16:10:30', '2024-11-25 16:10:30', 'developer'),
-  ('dev-disable', uuid_generate_v4(), 'Disabled account for local dev', false, false, '2024-11-25 16:10:30', '2024-11-25 16:10:30', 'developer'),
-  ('dev-write-only', uuid_generate_v4(), 'Write only account for local dev', false, true, '2024-11-25 16:10:30', '2024-11-25 16:10:30', 'developer');
+  INSERT INTO u_fws.api_key(account_id, key, account_name, read, write, date_created, date_modified, modified_by) VALUES 
+    ('dev-write', uuid_generate_v4(), 'Read and write account for local dev', true, true, NOW() AT  TIME ZONE 'UTC', NOW() AT  TIME ZONE 'UTC', modified_by),
+    ('dev-read', uuid_generate_v4(), 'Read account for local dev', true, false, NOW() AT  TIME ZONE 'UTC', NOW() AT  TIME ZONE 'UTC', modified_by),
+    ('dev-disable', uuid_generate_v4(), 'Disabled account for local dev', false, false, NOW() AT  TIME ZONE 'UTC', NOW() AT  TIME ZONE 'UTC', modified_by),
+    ('dev-write-only', uuid_generate_v4(), 'Write only account for local dev', false, true, NOW() AT  TIME ZONE 'UTC', NOW() AT  TIME ZONE 'UTC', modified_by);
+END $$;
