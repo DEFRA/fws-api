f47a458e0932f4da9538bd34fe912f6b3324ce59
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-11-26 11:01:12 +0000

    Register Lambda functions and populate API keys

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -6,5 +6,7 @@ sudo .devcontainer/scripts/install-packages.sh
 .devcontainer/scripts/setup-aws-cli-command-completion.sh
 .devcontainer/scripts/install-maven.sh
 .devcontainer/scripts/apply-liquibase-changesets.sh
+docker exec -it fws-api_devcontainer-fwsdb-1 psql -d $(cat /run/secrets/fws_db_name) -f /tmp/populate-api-keys.sql
 .devcontainer/scripts/init-npm.sh
+.devcontainer/scripts/create-lambda-functions.sh
 docker image prune -f
