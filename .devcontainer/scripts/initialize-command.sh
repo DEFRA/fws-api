cae738e97350107d2680ea0f015d0e5caeac58f7
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2024-12-20 16:16:47 +0000

    Correct comments and whitespace

--- .devcontainer/scripts/initialize-command.sh
+++ .devcontainer/scripts/initialize-command.sh
@@ -7,13 +7,14 @@ set -e
 # the host machine - see https://github.com/microsoft/vscode-remote-release/issues/6891
 #
 # The bootstrap container does not have access to custom environment variables on
-# the host machine and this appears to prevent sourcing docker secets from
+# the host machine and this appears to prevent sourcing docker secrets from
 # custom host machine environment variables accordingly.
 #
 # As a workaround, docker secrets are sourced from files on the host machine and use of limited
 # environment variables that are available to the bootstrap container (such as HOME).
 # This results in an ability to source docker secrets from the host machine without
-# reliance on cloning the repository to the filesystem of the host machine.
+# reliance on creating a dev container from a local repository on the filesystem
+# of the host machine.
 #
 # While there are no such problems when creating a dev contaner from a local repository,
 # cloning a repository into a container volume is recommended to improve performance
--- docker/fws-db/populate-api-keys.sql
+++ docker/fws-db/populate-api-keys.sql
@@ -4,7 +4,7 @@ DECLARE
 BEGIN
   TRUNCATE TABLE u_fws.api_key;
 
-  INSERT INTO u_fws.api_key(account_id, key, account_name, read, write, date_created, date_modified, modified_by) VALUES 
+  INSERT INTO u_fws.api_key(account_id, key, account_name, read, write, date_created, date_modified, modified_by) VALUES
     ('dev-write', uuid_generate_v4(), 'Read and write account for local dev', true, true, NOW() AT  TIME ZONE 'UTC', NOW() AT  TIME ZONE 'UTC', modified_by),
     ('dev-read', uuid_generate_v4(), 'Read account for local dev', true, false, NOW() AT  TIME ZONE 'UTC', NOW() AT  TIME ZONE 'UTC', modified_by),
     ('dev-disable', uuid_generate_v4(), 'Disabled account for local dev', false, false, NOW() AT  TIME ZONE 'UTC', NOW() AT  TIME ZONE 'UTC', modified_by),
