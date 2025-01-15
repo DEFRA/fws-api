10285f3af3081a61a581489e86533eea3f6f383f
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-01-15 16:24:39 +0000

    Add unit tests

--- test/helpers/config.js
+++ test/helpers/config.js
@@ -1,2 +1,22 @@
-// TODO
-// Test that config is all present and correct
+const Lab = require('@hapi/lab')
+const lab = exports.lab = Lab.script()
+const Code = require('@hapi/code')
+
+lab.experiment('Config test', () => {
+  lab.beforeEach(() => {
+    process.env.FWS_ENV_NAME = 'tst'
+  })
+
+  lab.afterEach(() => {
+    delete require.cache[require.resolve('../../lib/config')]
+  })
+
+  lab.test('Check valid config passes', () => {
+    Code.expect(() => { require('../../lib/config') }).not.to.throw()
+  })
+
+  lab.test('Check bad config fails', () => {
+    process.env.FWS_ENV_NAME = 'unknown'
+    Code.expect(() => { require('../../lib/config') }).to.throw()
+  })
+})
