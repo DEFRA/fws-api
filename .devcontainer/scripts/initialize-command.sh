0e6f8fba65ccc3c4b87b57b3cbd7724a5a3dcd38
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-01-22 16:31:47 +0000

    Update comments

--- .devcontainer/scripts/initialize-command.sh
+++ .devcontainer/scripts/initialize-command.sh
@@ -3,24 +3,18 @@
 set -e
 
 # NOTE - When running a dev container from a repository cloned into a container
-# volume this script runs in a container used for bootstrapping rather than
+# volume this script runs in a container used for bootstrapping rather than on
 # the host machine - see https://github.com/microsoft/vscode-remote-release/issues/6891
 #
 # The bootstrap container does not have access to custom environment variables on
-# the host machine and this appears to prevent sourcing docker secrets from
-# custom host machine environment variables accordingly.
+# the host machine and the dev container .env file MUST be used to configure
+# non-sensitive environment variables/well known secrets accordingly.
 #
-# As a workaround, docker secrets are sourced from files on the host machine and use of limited
-# environment variables that are available to the bootstrap container (such as HOME).
-# This results in an ability to source docker secrets from the host machine without
-# reliance on creating a dev container from a local repository on the filesystem
-# of the host machine.
-#
-# While there are no such problems when creating a dev contaner from a local repository,
-# cloning a repository into a container volume is recommended to improve performance
+# Cloning a repository into a container volume is recommended to improve performance
 # - see https://code.visualstudio.com/remote/advancedcontainers/improve-performance
 
 # Regardless of whether a dev container uses a container volume or a bind mount to a
-# local repository, named volumes are created on the host machne.
+# local repository, named volumes are created on the host machine.
+
 docker volume create pgdata
 docker volume create pgadmin
