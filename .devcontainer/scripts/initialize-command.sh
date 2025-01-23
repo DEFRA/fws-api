0d37fdeea6ee9343dd50aae7cc33bdaae5ad6ab4
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-01-23 11:11:01 +0000

    Support portable database bootstrapping

--- .devcontainer/scripts/initialize-command.sh
+++ .devcontainer/scripts/initialize-command.sh
@@ -8,17 +8,30 @@ set -e
 #
 # The bootstrap container does not have access to custom environment variables on
 # the host machine and the dev container .env file MUST be used to configure
-# non-sensitive environment variables/well known secrets accordingly.
+# non-sensitive environment variables and well known secrets accordingly.
 #
 # Cloning a repository into a container volume is recommended to improve performance
 # - see https://code.visualstudio.com/remote/advancedcontainers/improve-performance
 
+# IMPORTANT
 # Regardless of whether a dev container uses a container volume or a bind mount to a
-# local repository, named volumes are created on the host machine.
-
+# local repository, named volumes are created on the host machine. Named volumes
+# ensure portability across the host plaforms used to create and run dev containers.
 docker volume create pgdata
 docker volume create pgadmin
 docker volume create pgbootstrap
+
+# Default to configuration required when creating a dev container by cloning the remote
+# repository into a container volume.
+HOST_DIR=/workspaces/fws-api
+
+if [ !-d ${HOST_DIR} && -d /opt{$HOST_DIR} ]; then
+  # A dev container is being created from a local repository.
+  HOST_DIR=/opt{$HOST_DIR}
+fi
+
+# Create a temporay container to load the database bootstrapping script into a named volume
+# used by the database container.
 docker container create --name temp -v pgbootstrap:/docker-entrypoint-initdb.d alpine
-docker cp /opt/workspaces/fws-api/docker/fws-db/bootstrap-fws-db.sh temp:/docker-entrypoint-initdb.d/bootstrap-fws-db.sh
+docker cp ${HOST_DIR}/docker/fws-db/bootstrap-fws-db.sh temp:/docker-entrypoint-initdb.d/bootstrap-fws-db.sh
 docker rm temp
