20351ac6fb8dbfd374c2389d39830d20f894c84b
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-01-23 09:45:08 +0000

    Remove pgbootstrap volume

--- .devcontainer/scripts/remove-volumes.sh
+++ .devcontainer/scripts/remove-volumes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-docker volume rm -f pgadmin pgdata vscode
+docker volume rm -f pgadmin pgdata vscode pgbootstrap
 docker volume rm -f $(docker volume ls -f name=fws --format json | jq -r .Name)
