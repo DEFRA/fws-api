27ca09d8813df8f00dfb3aa7bcf63fa337b71a68
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-02-03 17:42:35 +0000

    Improve volume management

--- .devcontainer/scripts/initialize-command.sh
+++ .devcontainer/scripts/initialize-command.sh
@@ -17,9 +17,28 @@ set -e
 # Regardless of whether a dev container uses a container volume or a bind mount to a
 # local repository, named volumes are created on the host machine. Named volumes
 # ensure portability across the host plaforms used to create and run dev containers.
-docker volume create pgdata
-docker volume create pgadmin
-docker volume create pgbootstrap
+
+PGDATA_VOLUME=$(docker volume ls -q -f "name=pgdata")
+PGADMIN_VOLUME=$(docker volume ls -q -f "name=pgadmin")
+PGBOOTSTRAP_VOLUME=$(docker volume ls -q -f "name=pgbootstrap")
+
+if [ -z "$PGDATA_VOLUME" ]; then
+  docker volume create pgdata
+else
+  echo Named volume pgdata exists
+fi
+
+if [ -z "$PGADMIN_VOLUME" ]; then
+  docker volume create pgadmin
+else
+  echo Named volume pgadmin exists
+fi
+
+if [ -z "$PGBOOTSTRAP_VOLUME" ]; then
+  docker volume create pgbootstrap
+else
+  echo Named volume pgbootstrap exists
+fi
 
 # Default to configuration required when creating a dev container by cloning the remote
 # repository into a container volume.
@@ -30,9 +49,19 @@ if [ ! -d ${HOST_DIR} ] && ([ -d /opt${HOST_DIR} ] || [ -L /opt${HOST_DIR} ]); t
   HOST_DIR=/opt${HOST_DIR}
 fi
 
+TEMP_CONTAINER=$(docker ps -a -q -f "name=temp")
+
+if [ ! -z "$TEMP_CONTAINER" ]; then
+  docker rm temp
+  echo Removed temp container
+fi
+
 # Create a temporay container to load the database bootstrapping script into a named volume
 # used by the database container
 # https://stackoverflow.com/questions/37468788/what-is-the-right-way-to-add-data-to-an-existing-named-volume-in-docker
 docker container create --name temp -v pgbootstrap:/docker-entrypoint-initdb.d alpine
+echo Created temp container
 docker cp ${HOST_DIR}/docker/fws-db/bootstrap-fws-db.sh temp:/docker-entrypoint-initdb.d/bootstrap-fws-db.sh
 docker rm temp
+echo Removed temp container
+
