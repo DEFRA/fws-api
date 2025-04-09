cc77f236efb6b894fde5f223428b05b8dc987ee2
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-04-09 21:06:15 +0100

    Correct logic

--- docker/scripts/post-create-command.sh
+++ docker/scripts/post-create-command.sh
@@ -10,7 +10,7 @@ docker/scripts/register-lambda-functions.sh
 docker/scripts/register-api-gateway.sh
 docker exec -it docker-fwsdb-1 bash -c "(cd /tmp && psql -d ${FWS_DB_NAME} -f ./populate-api-keys.sql -f ./target_area_load.sql)"
 
-if [ -d /opt/workspaces/fws-api/docker ]  &&  [`stat -c "%U:%G" /opt/workspaces/fws-api/docker` != vscode:vscode ]; then
+if [ -d /opt/workspaces/fws-api/docker ]; then
   # Ensure that docker directory contents can be modified from within the development container.
   sudo chown -R vscode:vscode /opt/workspaces/fws-api/docker
 fi
