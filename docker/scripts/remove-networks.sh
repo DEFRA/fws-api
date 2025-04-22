14817072b028d12c09a1ba64df524166e579bd3a
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-04-22 15:33:00 +0100

    Correct typo

--- docker/scripts/remove-networks.sh
+++ docker/scripts/remove-networks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-FWS_NETWORK=$(docker network ls -q -f "name=fws")
+FWS_NETWORK=$(docker network ls -q -f "name=docker_ls")
 
 if [ ! -z "$FWS_NETWORK" ]; then
-  docker network rm -f $(docker network ls -f name=fws --format json | jq -r .Name)
+  docker network rm -f $(docker network ls -f name=docker_ls --format json | jq -r .Name)
 fi
