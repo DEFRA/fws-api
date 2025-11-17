0d1ab39eaa9721b3c6a4333db7573ebdf8a8e5eb
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-11-17 09:59:40 +0000

    Upgrade to Node.js 22

--- docker/scripts/register-lambda-functions.sh
+++ docker/scripts/register-lambda-functions.sh
@@ -23,7 +23,7 @@ for lambda_function in "$lambda_functions_dir"/*; do
       awslocal lambda create-function \
         --function-name "$function_name" \
         --code S3Bucket="hot-reload",S3Key="$(pwd)/" \
-        --runtime nodejs20.x \
+        --runtime nodejs${NODEJS_VERSION}.x \
         --timeout $LAMBDA_TIMEOUT \
         --role arn:aws:iam::000000000000:role/lambda-role \
         --handler lib/functions/$function_name.handler \
