17aec4522509ea6ec234e9a4b58f19a9d50fbf9b
Tedd Mason <teddmason@gmail.com>
2025-11-19 15:06:40 +0000

    Returning json data type for processMessage to support fws-app

--- docker/scripts/register-api-gateway.sh
+++ docker/scripts/register-api-gateway.sh
@@ -99,20 +99,6 @@ put_method_and_integration() {
   # As such, a conditional block is used to hardcode the request-templates parameter for the process-message
   # lambda function.
   if [ $lambda_function_name = "process-message" ]; then
-    # IMPORTANT
-    # Due to apparent processing differences between the real AWS API Gateway and the associated LocalStack emulator,
-    # the text/html request template for LocalStack AWS API Gateway emulation uses $input.json("$.message") rather than
-    # $input.json("$").
-    # This is to allow the application code to receive the same data as it would from the real AWS API Gateway
-    # when client requests are submitted using the content type text/html. When using the content type text/html,
-    # XML data submitted to the AWS API gateway LocalStack emulator MUST be wrapped in a JSON object of the
-    # form {"message": "<xml data with double quote escaping>"}. This results in the Lambda function receiving
-    # JSON of the form {"bodyXml": "<xml data with double quote escaping>"}.
-    # JSON of the form {"bodyXml": "<xml data with double quote escaping>"} can also be submitted to the
-    # AWS API Gateway LocalStack emulator directly using the application/json content type. Use of the request template
-    # $input.body for the content type application/json ensures that the request payload is passed to the Lambda function
-    # unaltered. In contrast, the real AWS API Gateway achieves the same effect using an empty request template for the
-    # content type application/json.
     awslocal apigateway put-integration \
       --rest-api-id $fws_rest_api_id \
       --resource-id $resource_id \
@@ -122,7 +108,8 @@ put_method_and_integration() {
       --uri arn:aws:apigateway:eu-west-2:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-west-2:000000000000:function:$lambda_function_name/invocations \
       --passthrough-behavior WHEN_NO_TEMPLATES \
       --request-templates '{
-            "text/html": "{\"bodyXml\": \"$util.escapeJavaScript($input.body)\"}"
+            "text/html": "{\"bodyXml\": \"$util.escapeJavaScript($input.body)\"}",
+            "application/json": "$input.body"
           }'
 
     # AWS integrations require integration responses to be set manually.
